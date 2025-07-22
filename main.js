f6448d4bee196afa74e59461c00845708544723a
Ahmed Moatemed <https://githup.com/ahmed-moatemed>
2025-07-22 19:46:34 +0300

    random ayah

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+const btn = document.querySelector('.click-button');
+const ayah = document.querySelector('.ayah');
+const surah = document.querySelector('.surah');
+const ayahNum = document.querySelector('.num-ayah');
+
+btn.addEventListener('click', () => {
+  getAyah();
+});
+
+async function getAyah() {
+  const verseNumber = Math.floor(Math.random() * 6236) + 1;
+  const response = await fetch(`http://api.alquran.cloud/v1/ayah/${verseNumber}`);
+   try{ 
+    let data = await response.json(); 
+
+    ayah.innerHTML = data.data.text;
+    surah.innerHTML = data.data.surah.name;
+    ayahNum.innerHTML =  ' الآية ' + data.data.numberInSurah ;
+    
+  } catch (error) {
+    alert('Error');
+  }
+
+}
